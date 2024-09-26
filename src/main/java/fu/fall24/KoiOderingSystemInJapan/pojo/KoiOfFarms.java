98cd69f6df813414079ba7e68efcd9dc68e0606b
kphan <khoipmse182554@fpt.edu.vn>
2024-09-26 15:18:41 +0700

    add

---
+++ src/main/java/fu/fall24/KoiOderingSystemInJapan/pojo/KoiOfFarms.java
@@ -0,0 +1,14 @@
+package fu.fall24.KoiOderingSystemInJapan.pojo;
+
+public class KoiOfFarms {
+
+    private int id;
+
+    private int farmId;
+
+    private int koiId;
+
+    private int quantity;
+
+    private int available;
+}
---
+++ src/test/java/fu/fall24/KoiOderingSystemInJapan/KoiOderingSystemInJapanApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.fall24.KoiOderingSystemInJapan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiOderingSystemInJapanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
